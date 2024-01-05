563e119d472876218978de04e7a0efc7659002bb
Luofan Chen <clfbbn@gmail.com>
2024-01-05 20:39:39 +0530

    lotus: Ship android.system.keystore2
    Fixes: Could not find 'aidl/android.system.keystore2.IKeystoreService/default'
           for ctl.interface_start from pid: 617 (/system/bin/servicemanager)
    
    Signed-off-by: Alen 24 <alenbijuk2003@gmail.com>

--- device.mk
+++ device.mk
@@ -64,7 +64,8 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-service \
     android.hardware.keymaster@3.0-impl \
     libkeystore-engine-wifi-hidl \
-    libkeystore-wifi-hidl
+    libkeystore-wifi-hidl \
+    android.system.keystore2
 
 # Light
 PRODUCT_PACKAGES += \
